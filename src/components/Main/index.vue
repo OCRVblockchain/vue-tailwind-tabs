fa19b161c5141101ff526ba0819646c827b84b2a
Denis Kozlov <denis.kozlov94@icloud.com>
2021-01-29 17:12:49 +0300

    implementing Toasts (WIP)

--- src/components/Main/index.vue
+++ src/components/Main/index.vue
@@ -1,7 +1,9 @@
 <template>
     <main class="min-h-screen bg-blue-50">
-      <SuccessModal :isOpen="isOpenSuccessModal" :ok="hideModals" :cancel="hideModals" title="Регистрация пройдена" msg="Вы успешно зарегистрированы и можете начинать работу." />
-      <WarningModal :isOpen="isOpenWarningModal" :ok="hideModals" :cancel="hideModals" title="Смена пароля" msg="Для продолжения, необходимо ваше подтверждение." />
+      <Toasts />
+
+      <SuccessModal :isOpen="isOpenSuccessModal" :ok="hideModals" :cancel="hideModals" title="Регистрация пройдена" msg="Вы успешно зарегистрированы и можете начать работу." />
+      <WarningModal :isOpen="isOpenWarningModal" :ok="hideModals" :cancel="hideModals" title="Смена пароля" msg="Для продолжения, необходимо ваше подтверждение. " />
       <DangerModal :isOpen="isOpenDangerModal" :ok="hideModals" :cancel="hideModals" title="Удаление аккаунта" msg="Это действие будет невозможно отменить." />
 
       <nav>
@@ -14,9 +16,15 @@
         </aside>
 
         <div class="h-full ml-5">
-          <button type="button" @click="isOpenSuccessModal = true" class="focus:outline-none bg-green-600 text-white px-8 py-2 rounded-lg">Регистрация</button>
-          <button type="button" @click="isOpenWarningModal = true" class="focus:outline-none bg-yellow-600 text-white ml-3 px-8 py-2 rounded-lg">Смена пароля</button>
-          <button type="button" @click="isOpenDangerModal = true" class="focus:outline-none bg-red-600 text-white ml-3 px-8 py-2 rounded-lg">Удаление аккаунта</button>
+          <p>
+            <button type="button" @click="isOpenSuccessModal = true" class="focus:outline-none bg-green-600 text-white px-8 py-2 rounded-lg">Регистрация</button>
+            <button type="button" @click="isOpenWarningModal = true" class="focus:outline-none bg-yellow-400 text-white ml-3 px-8 py-2 rounded-lg">Смена пароля</button>
+            <button type="button" @click="isOpenDangerModal = true" class="focus:outline-none bg-red-600 text-white ml-3 px-8 py-2 rounded-lg">Удаление аккаунта</button>
+          </p>
+          <p>
+            <button type="button" @click="sendMessage()" class="focus:outline-none bg-blue-600 text-white mt-3 px-8 py-2 rounded-lg">Отправить уведомление</button>
+          </p>
+
         </div>
       </section>
 
@@ -28,6 +36,7 @@ import {ref, defineComponent} from 'vue'
 import Navbar from '../Navbar/index.vue'
 import Sidebar from '../Sidebar/index.vue'
 import { DangerModal, WarningModal, SuccessModal } from '../Modals/index.ts'
+import Toasts from "../Toasts/index.vue";
 
 export default defineComponent({
   name: 'Main',
@@ -36,24 +45,35 @@ export default defineComponent({
     Sidebar,
     DangerModal,
     WarningModal,
-    SuccessModal
+    SuccessModal,
+    Toasts
   },
-  setup: () => {
-    const isOpen = ref(false)
-    const toggle = () => {
-        isOpen.value = !isOpen.value
+  data() {
+    return {
+      isOpen: false,
+      isOpenDangerModal: false,
+      isOpenWarningModal: false,
+      isOpenSuccessModal: false
     }
+  },
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen
+    },
+    hideModals() {
+      this.isOpenDangerModal = false
+      this.isOpenWarningModal = false
+      this.isOpenSuccessModal = false
+    },
+    sendMessage() {
+      const toast= {
+        title: 'Тест',
+        text: 'Какой-то текст',
+        type: 'warning'
+      }
 
-    const isOpenDangerModal = ref(false)
-    const isOpenWarningModal = ref(false)
-    const isOpenSuccessModal = ref(false)
-    const hideModals = () => {
-      isOpenDangerModal.value = false
-      isOpenWarningModal.value = false
-      isOpenSuccessModal.value = false
+      this.emitter.emit('toast', toast)
     }
-
-    return {isOpen, toggle, hideModals, isOpenDangerModal, isOpenWarningModal, isOpenSuccessModal}
   }
 })
 </script>
--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.css'
+import mitt from 'mitt';
 
-createApp(App).use(store).use(router).mount('#app')
+const emitter = mitt();
+let app=createApp(App)
+app.config.globalProperties.emitter = emitter
+app.use(store)
+app.use(router)
+app.mount('#app')
+
